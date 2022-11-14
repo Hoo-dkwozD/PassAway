cd34d204d289c6997f453ab5a3bec4a3c80834db
ZheTao <zacharytao99@gmail.com>
2022-11-14 17:46:27 +0800

    analyticsview

--- record-ui/src/main.ts
+++ record-ui/src/main.ts
@@ -4,6 +4,8 @@ import { VueCookie } from "vue-cookie";
 
 import App from "./App.vue";
 import router from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 import "./assets/main.css";
 import VCalendar from 'v-calendar'
